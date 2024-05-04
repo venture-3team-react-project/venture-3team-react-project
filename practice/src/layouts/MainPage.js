ffd9bb65e5a35cf398f30f0ee1232da236c7920f
SeokYong <132437309+Yoon0917@users.noreply.github.com>
2024-05-04 18:59:54 +0900

    aaa

--- practice/src/layouts/MainPage.js
+++ practice/src/layouts/MainPage.js
@@ -1,53 +1,57 @@
 import React from 'react';
 import ModelViewer from '../components/3d';
 import './MainPage.css';
+import SelectBarebone from '../components/SelectParts/SelectBarebone.js';
+import SelectSwitch from '../components/SelectParts/SelectSwitch.js';
+import SelectKeycap from '../components/SelectParts/SelectKeycap.js';
+import { useState } from 'react';
 
 const MainPage = () => {
+  const [barebone, setBarebone] = useState(''); // 베어본
+  const handleBareboneSelect = (value) => {
+      setBarebone(value);
+  };
+
+  const [keySwitch, setKeySwitch] = useState(''); // 베어본
+  const handlekeySwitchSelect = (value) => {
+      setKeySwitch(value);
+  };
+
+  const [keycap, setKeycap] = useState(''); // 베어본
+  const handlekeycapSelect = (value) => {
+      setKeycap(value);
+  };
+
+
+
+
+
     return (
         <>
         <div className="App">
           <header>헤더입니다.</header>
           <main>
             <aside>
-              <div className="sel">베어본<br />
-                <form action="">
-                  <input type="radio" value="알루" />알루
-                  <input type="radio" value="플라" />플라
-                </form>
-                <select name="" id="">
-                  <option value="">선택</option>
-                  <option value="">알루</option>
-                  <option value="">플라</option>
-                </select>
+              <header>헤더입니다.</header>
+
+              <div className="selectOption">
+                <SelectBarebone onBareboneSelect={handleBareboneSelect}/>
               </div>
-              <div className="sel">스위치<br />
-                <select name="" id="">
-                  <option value="">선택</option>
-                  <option value="">청축</option>
-                  <option value="">적축</option>
-                </select>
+              <div className="selectOption">
+                <SelectSwitch onKeySwitchSelect={handlekeySwitchSelect}/>
               </div>
-              <div className="sel">키캡<br />
-                <select name="" id="">
-                  <option value="">선택</option>
-                  <option value="">pbt</option>
-                  <option value="">alt</option>
-                </select>
-              </div>
-              <div className="sel">
-                <button>조합하기</button>
+              <div className="selectOption">
+                <SelectKeycap onKeycapSelect={handlekeycapSelect}/>
               </div>
             </aside>
             <section>
               <div className="img">
-              
-              
-              <ModelViewer modelPath ="img/keyboard/scene.gltf"/>
-                </div>
-              <div className="info">정보<br />
-                <label htmlFor="barebone">베어본 : </label> <br />
-                <label htmlFor="switch">스위치 : </label> <br />
-                <label htmlFor="keycap">키캡 : </label> <br />
+                <ModelViewer modelPath ="img/keyboard/scene.gltf"/>
+              </div>
+              <div className="info">선택하신 부품<br />
+                <label htmlFor="result-parts-label">베어본 : {barebone}</label> <br />
+                <label htmlFor="result-parts-label">스위치 : {keySwitch}</label> <br />
+                <label htmlFor="result-parts-label">키캡 :  {keycap}</label> <br />
               </div>
             </section>
           </main>
